fe1b6343f45db4094782aa883d6c8b613006bd0b
abdonkov <baledo@abv.bg>
2016-08-18 22:17:13 +0300

    Added unit test for ArrayList

---
+++ DSA/DSAUnitTests/DataStructures/Lists/ArrayListTests.cs
@@ -0,0 +1,329 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSA.DataStructures.Lists;
+using System.Linq;
+
+namespace DSAUnitTests.DataStructures.Lists
+{
+    [TestClass]
+    public class ArrayListTests
+    {
+        [TestMethod]
+        public void AddingItemsOneByOne()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            Assert.IsTrue(list.Count == itemCount);
+        }
+
+        [TestMethod]
+        public void AddingRangeOfItems()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            var list2 = new ArrayList<int>();
+
+            list2.AddRange(list);
+
+            Assert.IsTrue(list.Count == itemCount
+                            && list2.Count == itemCount);
+        }
+
+        [TestMethod]
+        public void InitializingArrayListWithCollection()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            var list2 = new ArrayList<int>(list);
+
+            Assert.IsTrue(list.Count == itemCount
+                            && list2.Count == itemCount);
+        }
+
+        [TestMethod]
+        public void RemovingAllExceptOne()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            for (int i = 0; i < itemCount -1; i++)
+            {
+                list.Remove(i);
+            }
+
+            Assert.IsTrue(list.Count == 1
+                            && list[0] == itemCount - 1);
+        }
+
+        [TestMethod]
+        public void InitializationWithZeroCapacityAndAddingItemsAfterwards()
+        {
+            var list = new ArrayList<int>(0);
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            Assert.IsTrue(list.Count == itemCount);
+        }
+
+        [TestMethod]
+        public void RemovingAllItemsAndAddingAgain()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Remove(i);
+            }
+
+            bool countWasZero = list.Count == 0;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            Assert.IsTrue(list.Count == itemCount
+                            && countWasZero);
+        }
+
+        [TestMethod]
+        public void RemoveRangeOfItems()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            list.RemoveRange(0, itemCount - 1);
+
+            Assert.IsTrue(list.Count == 1
+                            && list[0] == itemCount - 1);
+        }
+
+        [TestMethod]
+        public void InsertAllItemsAtTheBeginningAndCheckForReversedOrder()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Insert(0, i);
+            }
+
+            var list2 = new ArrayList<int>(list.Reverse());
+
+            bool areReversed = true;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                if (list[i] != list2[itemCount - 1 - i])
+                {
+                    areReversed = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(list.Count == itemCount
+                            && list2.Count == itemCount
+                            && areReversed);
+        }
+
+        [TestMethod]
+        public void RemoveAtZeroUntilOneItemIsLeft()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            for (int i = 1; i < itemCount; i++)
+            {
+                list.RemoveAt(0);
+            }
+
+            Assert.IsTrue(list.Count == 1
+                            && list[0] == itemCount - 1);
+        }
+
+        [TestMethod]
+        public void IndexOfAndLastIndexOfTest()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+                list.Insert(0, i);
+            }
+
+            bool allInPlace = true;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                int first = list.IndexOf(i);
+                int last = list.LastIndexOf(i);
+
+                if (last - first - 1 - 2*i != 0)
+                {
+                    allInPlace = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(list.Count == itemCount * 2
+                            && allInPlace);
+        }
+
+        [TestMethod]
+        public void CheckIfContainedBeforeAndAfterRemoval()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            bool containsWorksFine = true;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                if (!list.Contains(i))
+                {
+                    containsWorksFine = false;
+                    break;
+                }
+                list.Remove(i);
+                if (list.Contains(i))
+                {
+                    containsWorksFine = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(list.Count == 0
+                            && containsWorksFine);
+        }
+
+        [TestMethod]
+        public void AddingAfterClearingCollection()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            list.Clear();
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            bool everythingIntact = true;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                if (list[i] != i)
+                {
+                    everythingIntact = false;
+                    break;
+                }
+            }
+
+            Assert.IsTrue(list.Count == itemCount
+                            && everythingIntact);
+        }
+
+        [TestMethod]
+        public void InsertingRangeOfItemsAtTheMiddle()
+        {
+            var list = new ArrayList<int>();
+
+            int itemCount = 100;
+
+            for (int i = 0; i < itemCount; i++)
+            {
+                list.Add(i);
+            }
+
+            var list2 = new ArrayList<int>(list);
+
+            list.InsertRange(itemCount / 2, list2);
+
+            for (int i = 0; i < itemCount / 2; i++)
+            {
+                if (list[i] != i) Assert.Fail();
+            }
+
+            for (int i = itemCount / 2; i < itemCount + itemCount / 2; i++)
+            {
+                if (list[i] != i - itemCount / 2) Assert.Fail();
+            }
+
+            for (int i = itemCount + itemCount / 2; i < itemCount * 2; i++)
+            {
+                if (list[i] != i - itemCount) Assert.Fail();
+            }
+
+            Assert.IsTrue(list.Count == itemCount * 2);
+        }
+    }
+}
