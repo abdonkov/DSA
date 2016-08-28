d26ae2e29ee448e75d1e2bcc2f658b2b499f663b
abdonkov <baledo@abv.bg>
2016-08-28 18:53:50 +0300

    Added Splay Tree unit test

---
+++ DSA/DSAUnitTests/DataStructures/Trees/SplayTreeTests.cs
@@ -0,0 +1,396 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using DSA.DataStructures.Trees;
+using System.Diagnostics;
+
+namespace DSAUnitTests.DataStructures.Trees
+{
+    [TestClass]
+    public class SplayTreeTests
+    {
+        [TestMethod]
+        public void SortedElementsAfterAdding()
+        {
+            var tree = new SplayTree<int>();
+
+            int elementsCount = 10000;
+
+            //To make it more balanced
+            tree.Add(50);
+            tree.Add(75);
+            tree.Add(25);
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el)) tree.Add(el);
+                    el += i;
+                }
+
+            }
+
+            int last = -1;
+            int count = 0;
+            bool elementsAreSorted = true;
+            foreach (var item in tree)
+            {
+                if (last > item)
+                {
+                    elementsAreSorted = false;
+                }
+                last = item;
+                count++;
+            }
+
+            Assert.IsTrue(tree.Count == elementsCount
+                            && elementsAreSorted
+                            && count == elementsCount);
+        }
+
+        [TestMethod]
+        public void SortedElementsAfterAddingAndRemoving()
+        {
+            var tree = new SplayTree<int>();
+
+            int elementsCount = 10000;
+
+            //To make it more balanced
+            tree.Add(50);
+            tree.Add(75);
+            tree.Add(25);
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el)) tree.Add(el);
+                    el += i;
+                }
+
+            }
+
+            bool removedEverything = true;
+
+            //Removing every second number
+            for (int i = 0; i < elementsCount; i += 2)
+            {
+                if (!tree.Remove(i)) removedEverything = false;
+            }
+
+
+            int last = -1;
+            int count = 0;
+            bool elementsAreSorted = true;
+            foreach (var item in tree)
+            {
+                if (last > item)
+                {
+                    elementsAreSorted = false;
+                }
+                last = item;
+                count++;
+            }
+
+            Trace.WriteLine(tree.Count);
+            Trace.WriteLine(count);
+            Trace.WriteLine(removedEverything);
+            Trace.WriteLine(elementsAreSorted);
+
+            Assert.IsTrue(tree.Count == count
+                            && elementsAreSorted
+                            && removedEverything);
+        }
+
+        [TestMethod]
+        public void RemoveAllExceptOne()
+        {
+            var tree = new SplayTree<int>();
+
+            int elementsCount = 10000;
+
+            //To make it more balanced
+            tree.Add(50);
+            tree.Add(75);
+            tree.Add(25);
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el)) tree.Add(el);
+                    el += i;
+                }
+
+            }
+
+            bool removedEverything = true;
+
+            for (int i = 0; i < elementsCount - 1; i++)
+            {
+                if (!tree.Remove(i)) removedEverything = false;
+            }
+
+            Assert.IsTrue(tree.Count == 1
+                            && removedEverything
+                            && tree.Root.Value == elementsCount - 1);
+        }
+
+        [TestMethod]
+        public void RemoveRootEveryTimeUntilTreeElementsAreHalfed()
+        {
+            var tree = new BinarySearchTree<int>();
+
+            int elementsCount = 10000;
+
+            //To make it more balanced
+            tree.Add(50);
+            tree.Add(75);
+            tree.Add(25);
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el))
+                    {
+                        tree.Add(el);
+                    }
+                    el += i;
+                }
+
+            }
+
+            bool removedEverything = true;
+
+            while (tree.Count > elementsCount / 2)
+            {
+                if (!tree.Remove(tree.Root.Value)) removedEverything = false;
+            }
+
+            int last = -1;
+            int count = 0;
+            bool elementsAreSorted = true;
+            foreach (var item in tree)
+            {
+                if (last > item)
+                {
+                    elementsAreSorted = false;
+                }
+                last = item;
+                count++;
+            }
+
+            Assert.IsTrue(tree.Count == count
+                            && elementsAreSorted
+                            && count == elementsCount / 2
+                            && removedEverything);
+        }
+
+        [TestMethod]
+        public void CheckIfTreeIsSplayedAfterAddAndAfterContains()
+        {
+            var tree = new SplayTree<int>();
+
+            int elementsCount = 10000;
+
+            //To make it more balanced
+            tree.Add(50);
+            if (tree.Root.Value != 50) Assert.Fail();
+
+            tree.Add(75);
+            if (tree.Root.Value != 75) Assert.Fail();
+
+            tree.Add(25);
+            if (tree.Root.Value != 25) Assert.Fail();
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el))
+                    {
+                        tree.Add(el);
+                        if (tree.Root.Value != el) Assert.Fail();
+
+                    }
+                    else if (tree.Root.Value != el) Assert.Fail();
+
+                    el += i;
+                }
+
+            }
+
+            int last = -1;
+            int count = 0;
+            bool elementsAreSorted = true;
+            foreach (var item in tree)
+            {
+                if (last > item)
+                {
+                    elementsAreSorted = false;
+                }
+                last = item;
+                count++;
+            }
+
+            Assert.IsTrue(tree.Count == elementsCount
+                            && elementsAreSorted
+                            && count == elementsCount);
+        }
+
+        [TestMethod]
+        public void AddingAfterClearingTree()
+        {
+            var tree = new SplayTree<int>();
+
+            int elementsCount = 10000;
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el))
+                    {
+                        tree.Add(el);
+                    }
+                    el += i;
+                }
+
+            }
+
+            if (tree.Count != elementsCount) Assert.Fail();
+
+            tree.Clear();
+
+            if (tree.Count != 0) Assert.Fail();
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el))
+                    {
+                        tree.Add(el);
+                    }
+                    el += i;
+                }
+
+            }
+
+            int last = -1;
+            int count = 0;
+            bool elementsAreSorted = true;
+            foreach (var item in tree)
+            {
+                if (last > item)
+                {
+                    elementsAreSorted = false;
+
+                }
+                last = item;
+                count++;
+            }
+
+            Assert.IsTrue(tree.Count == elementsCount
+                            && elementsAreSorted
+                            && count == elementsCount);
+        }
+
+        [TestMethod]
+        public void AddingAfterRemovingAllElements()
+        {
+            var tree = new SplayTree<int>();
+
+            int elementsCount = 10000;
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el))
+                    {
+                        tree.Add(el);
+                    }
+                    el += i;
+                }
+
+            }
+
+            if (tree.Count != elementsCount) Assert.Fail();
+
+            //Removing every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    tree.Remove(el);
+                    el += i;
+                }
+
+            }
+
+            if (tree.Count != 0) Assert.Fail();
+
+            //Adding every seventh number, then every fifth number,
+            //every third and at last all numbers
+            for (int i = 7; i > 0; i -= 2)
+            {
+                int el = 0;
+                while (el < elementsCount)
+                {
+                    if (!tree.Contains(el))
+                    {
+                        tree.Add(el);
+                    }
+                    el += i;
+                }
+
+            }
+
+            int last = -1;
+            int count = 0;
+            bool elementsAreSorted = true;
+            foreach (var item in tree)
+            {
+                if (last > item)
+                {
+                    elementsAreSorted = false;
+
+                }
+                last = item;
+                count++;
+            }
+
+            Assert.IsTrue(tree.Count == elementsCount
+                            && elementsAreSorted
+                            && count == elementsCount);
+        }
+    }
+}
