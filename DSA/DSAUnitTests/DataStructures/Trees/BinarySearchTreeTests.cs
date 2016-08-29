02606423de0e7741d28f79e04379e1460d65f506
abdonkov <baledo@abv.bg>
2016-08-29 23:01:01 +0300

    Added unit test for node invalidation
    Added unit test for node invalidation when removing a node and when
    clearing the tree

--- DSA/DSAUnitTests/DataStructures/Trees/BinarySearchTreeTests.cs
+++ DSA/DSAUnitTests/DataStructures/Trees/BinarySearchTreeTests.cs
@@ -339,5 +339,49 @@ namespace DSAUnitTests.DataStructures.Trees
                             && elementsAreSorted
                             && count == elementsCount);
         }
+
+        [TestMethod]
+        public void CheckIfNodeIsInvalidatedAfterClearingAndAfterRemoval()
+        {
+            var tree = new BinarySearchTree<int>();
+
+            tree.Add(2);
+            tree.Add(1);
+            tree.Add(3);
+
+            // Tree looks like this:
+            //   2
+            //  / \
+            // 1   3
+
+            var node1 = tree.Root.Left;
+            var node2 = tree.Root;
+            var node3 = tree.Root.Right;
+
+            tree.Remove(2);
+            if (node2.Left != null || node2.Right != null) Assert.Fail("2");
+
+            tree.Remove(3);
+            if (node3.Left != null || node3.Right != null) Assert.Fail("3");
+
+            tree.Remove(1);
+            if (node1.Left != null || node1.Right != null) Assert.Fail("1");
+
+            tree.Add(2);
+            tree.Add(1);
+            tree.Add(3);
+
+            node1 = tree.Root.Left;
+            node2 = tree.Root;
+            node3 = tree.Root.Right;
+
+            tree.Clear();
+
+            Assert.IsTrue(node1.Left == null && node1.Right == null
+                            && node2.Left == null && node2.Right == null
+                            && node3.Left == null && node3.Right == null
+                            && tree.Root == null
+                            && tree.Count == 0);
+        }
     }
 }
