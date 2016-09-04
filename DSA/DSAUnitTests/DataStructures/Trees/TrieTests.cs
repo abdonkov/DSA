d106130f4e6f07f41dd5580d7543b797b1d7daea
abdonkov <baledo@abv.bg>
2016-09-04 22:24:40 +0300

    Added more Trie unit tests
    Added unit tests for checking if prefix is contained and getting words
    by prefix

--- DSA/DSAUnitTests/DataStructures/Trees/TrieTests.cs
+++ DSA/DSAUnitTests/DataStructures/Trees/TrieTests.cs
@@ -268,5 +268,82 @@ namespace DSAUnitTests.DataStructures.Trees
 
             Assert.IsTrue(trie.Count == words.Length - removedWords);
         }
+
+        [TestMethod]
+        public void CheckIfPrefixIsContained()
+        {
+            var trie = new Trie();
+
+            var words = new string[]
+            {
+                "One", "one", "oNe", "two", "hello",
+                "test", "here", "there", "?!??!?!?!?",
+                "VeryVeryVeryLoooooooooooooooooong",
+                ".k[2c3-9024g-u,9weg,ouimwt", "3q2tgwadh",
+                "`+rv`+*1v+vt23*1`vt*1v", "!@#)(*^$!%@_",
+                "  bum  ", "  bam  ", "  bum  bam  ",
+                "1", "12", "123", "1234", "12345", "123456"
+            };
+
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                trie.Add(words[i]);
+
+                for (int j = 1; j <= words[i].Length; j++)
+                {
+                    if (!trie.ContainsPrefix(words[i].Substring(0, j))) Assert.Fail();
+                }
+                if (!trie.Contains(words[i])) Assert.Fail();
+
+            }
+
+            Assert.IsTrue(trie.Count == words.Length);
+        }
+
+        [TestMethod]
+        public void GetWordsByPrefixAndCheckIfSorted()
+        {
+            var trie = new Trie();
+
+            var words = new string[]
+            {
+                "prefix afa", "prefix abaa", "prefix asd",
+                "prefix ase", "prefix rfda", "prefix rtfs",
+                "prefix efad", "prefix Abaa", "prefix fasrg",
+                "prefix test", "prefix t3st", "prefix tEst",
+                "prefix TEST", "prefix Test", "prefix T3st",
+                "prefix Test123", "prefix aheyj", "prefix gwecgs",
+                "prefix .,gml;k", "prefix rbkp;r", "prefix wopger'lb",
+                "prefix   zzz", "prefix ^&*(&", "prefix кирилица",
+                "no prefix"
+            };
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (trie.Contains(words[i])) Assert.Fail();
+
+                trie.Add(words[i]);
+
+                if (!trie.Contains(words[i])) Assert.Fail();
+
+            }
+
+            var previousWord = string.Empty;
+            int prefixWords = 0;
+
+            foreach (var word in trie.GetWordsByPrefix("prefix"))
+            {
+                if (string.CompareOrdinal(previousWord, word) > 0) Assert.Fail();
+
+                System.Diagnostics.Trace.WriteLine(word);
+
+                previousWord = word;
+                prefixWords++;
+            }
+
+            Assert.IsTrue(trie.Count == words.Length
+                            && prefixWords < words.Length);
+        }
     }
 }
