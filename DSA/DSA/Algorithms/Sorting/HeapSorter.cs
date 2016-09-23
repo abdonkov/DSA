7a5a56ebad8c14b5967bcd7975e8be8bce971041
Alexander Donkov <abdonkov@gmail.com>
2016-09-23 16:08:20 +0300

    Removed unnecessary usings

--- DSA/DSA/Algorithms/Sorting/HeapSorter.cs
+++ DSA/DSA/Algorithms/Sorting/HeapSorter.cs
@@ -1,9 +1,5 @@
-﻿using DSA.DataStructures.Heaps;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DSA.Algorithms.Sorting
 {
