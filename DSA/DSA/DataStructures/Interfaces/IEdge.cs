42e54119dafb38158693b5ee87db611afcf8dd4e
Alexander Donkov <abdonkov@gmail.com>
2016-10-06 22:41:15 +0300

    Added IEdge and IWeightedEdge interfaces

---
+++ DSA/DSA/DataStructures/Interfaces/IEdge.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DSA.DataStructures.Interfaces
+{
+    /// <summary>
+    /// The edge interface.
+    /// </summary>
+    /// <typeparam name="TVertex">The data type of the vertices. TVertex implements <see cref="IComparable{T}"/>.</typeparam>
+    public interface IEdge<TVertex>
+        where TVertex : IComparable<TVertex>
+    {
+        /// <summary>
+        /// Deteremines whether the edge is weighted.
+        /// </summary>
+        bool IsWeighted { get; }
+
+        /// <summary>
+        /// Gets the source vertex of the edge.
+        /// </summary>
+        TVertex Source { get; }
+
+        /// <summary>
+        /// Gets the destination vertex of the edge.
+        /// </summary>
+        TVertex Destination { get; }
+    }
+}
---
+++ DSA/DSA/DataStructures/Interfaces/IWeightedEdge.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace DSA.DataStructures.Interfaces
+{
+    /// <summary>
+    /// The weighted edge interface.
+    /// </summary>
+    /// <typeparam name="TVertex">The data type of the vertices. TVertex implements <see cref="IComparable{T}"/>.</typeparam>
+    /// <typeparam name="TWeight">The data type of the weight of the edge. TWeight implements <see cref="IComparable{T}"/>.</typeparam>
+    public interface IWeightedEdge<TVertex, TWeight> : IEdge<TVertex>
+        where TVertex : IComparable<TVertex>
+        where TWeight : IComparable<TWeight>
+    {
+        /// <summary>
+        /// Gets the weight of the edge.
+        /// </summary>
+        TWeight Weight { get; }
+    }
+}
