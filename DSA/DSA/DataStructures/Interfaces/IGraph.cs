29ce2e3fc7fd05e188eb61dc381d326b7efed2b0
Alexander Donkov <abdonkov@gmail.com>
2016-10-06 22:42:35 +0300

    Added IGraph and IWeightedGraph

---
+++ DSA/DSA/DataStructures/Interfaces/IGraph.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSA.DataStructures.Interfaces
+{
+    /// <summary>
+    /// The graph interface.
+    /// </summary>
+    /// <typeparam name="TVertex">The data type of the vertices. TVertex implements <see cref="IComparable{T}"/>.</typeparam>
+    public interface IGraph<TVertex>
+        where TVertex : IComparable<TVertex>
+    {
+        /// <summary>
+        /// Determines whether the graph is directed.
+        /// </summary>
+        bool IsDirected { get; }
+
+        /// <summary>
+        /// Deteremines whether the graph is weighted.
+        /// </summary>
+        bool IsWeighted { get; }
+
+        /// <summary>
+        /// Gets the number of edges in the graph.
+        /// </summary>
+        int EdgesCount { get; }
+
+        /// <summary>
+        /// Gets the number of vertices in the graph.
+        /// </summary>
+        int VerticesCount { get; }
+
+        /// <summary>
+        /// Adds an edge defined by the given existing vertices.
+        /// </summary>
+        /// <param name="firstVertex">The first vertex. Source of the edge if the graph is directed.</param>
+        /// <param name="secondVertex">The second vertex. Destination of the edge if the graph is directed.</param>
+        /// <returns>Returns true if the edge was added successfully; otherwise false. Also returns false if the vertices are not present in this graph or the edge already exists.</returns>
+        bool AddEdge(TVertex firstVertex, TVertex secondVertex);
+
+        /// <summary>
+        /// Adds a vertex to the graph.
+        /// </summary>
+        /// <param name="vertex">The vertex to add.</param>
+        /// <returns>Returns true if the edge was added successfully; otherwise false. Also returns false if the vertex already exists.</returns>
+        bool AddVertex(TVertex vertex);
+
+        /// <summary>
+        /// Returns the incoming edges of the given vertex.
+        /// </summary>
+        /// <param name="vertex">The vertex whose incoming edges are returned.</param>
+        /// <returns>Returns a <see cref="IList{T}"/> of <see cref="IEdge{TVertex}"/> of all incoming edges of the given vertex.</returns>
+        IList<IEdge<TVertex>> IncomingEdges(TVertex vertex);
+
+        /// <summary>
+        /// Returns the outgoing edges of the given vertex.
+        /// </summary>
+        /// <param name="vertex">The vertex whose outgoing edges are returned.</param>
+        /// <returns>Returns a <see cref="IList{T}"/> of <see cref="IEdge{TVertex}"/> of all outgoing edges of the given vertex.</returns>
+        IList<IEdge<TVertex>> OutgoingEdges(TVertex vertex);
+
+        /// <summary>
+        /// Determines whether the edge is presented in the graph.
+        /// </summary>
+        /// <param name="firstVertex">The first vertex of the edge. Source of the edge if the graph is directed.</param>
+        /// <param name="secondVertex">The second vertex of the edge. Destination of the edge if graph is directed.</param>
+        /// <returns>Returns true if the edge is presented in the graph; false otherwise.</returns>
+        bool ContainsEdge(TVertex firstVertex, TVertex secondVertex);
+
+        /// <summary>
+        /// Determines whether the vertex is presented in the graph.
+        /// </summary>
+        /// <param name="vertex">The vertex to see if presented in the graph.</param>
+        /// <returns>Returns true if the vertex is presented in the graph; false otherwise.</returns>
+        bool ContainsVertex(TVertex vertex);
+
+        /// <summary>
+        /// Removes the edge defined by the given vertices.
+        /// </summary>
+        /// <param name="firstVertex">The first vertex. Source of the edge if the graph is directed.</param>
+        /// <param name="secondVertex">The second vertex. Destination of the edge if the graph is directed.</param>
+        /// <returns>Returns true if the edge was removed successfully; otherwise false. Also returns false if the vertices are not present in this graph or the edge does not exist.</returns>
+        bool RemoveEdge(TVertex firstVertex, TVertex secondVertex);
+
+        /// <summary>
+        /// Removes the given vertex.
+        /// </summary>
+        /// <param name="vertex">The vertex to remove.</param>
+        /// <returns>Returns true if the vertex was removed successfully; otherwise false. Also returns false if the vertex does not exist.</returns>
+        bool RemoveVertex(TVertex vertex);
+
+        /// <summary>
+        /// Returns the degree of the given vertex.
+        /// </summary>
+        /// <param name="vertex">The vertex to calculate its degeree.</param>
+        /// <returns>Returns the degree of the given vertex.</returns>
+        int Degree(TVertex vertex);
+        
+        /// <summary>
+        /// Removes all edges and vertices from the graph.
+        /// </summary>
+        void Clear();
+    }
+}
---
+++ DSA/DSA/DataStructures/Interfaces/IWeightedGraph.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace DSA.DataStructures.Interfaces
+{
+    /// <summary>
+    /// The weighted graph interface.
+    /// </summary>
+    /// <typeparam name="TVertex">The data type of the vertices. TVertex implements <see cref="IComparable{T}"/>.</typeparam>
+    /// <typeparam name="TWeight">The data type of the weight of the edges. TWeight implements <see cref="IComparable{T}"/>.</typeparam>
+    public interface IWeightedGraph<TVertex, TWeight> : IGraph<TVertex>
+        where TVertex : IComparable<TVertex>
+        where TWeight : IComparable<TWeight>
+    {
+        /// <summary>
+        /// Adds an edge defined by the given existing vertices, having the given weight.
+        /// </summary>
+        /// <param name="firstVertex">The first vertex. Source of the edge if the graph is directed.</param>
+        /// <param name="secondVertex">The second vertex. Destination of the edge if the graph is directed.</param>
+        /// <param name="weight">The weight of the edge.</param>
+        /// <returns>Returns true if the edge was added successfully; otherwise false. Also returns false if the vertices are not present in this graph or the edge already exists.</returns>
+        bool AddEdge(TVertex firstVertex, TVertex secondVertex, TWeight weight);
+
+        /// <summary>
+        /// Returns the incoming edges of the given vertex.
+        /// </summary>
+        /// <param name="vertex">The vertex whose incoming edges are returned.</param>
+        /// <returns>Returns a <see cref="IList{T}"/> of <see cref="IWeightedEdge{TVertex, TWeight}"/> of all incoming edges of the given vertex.</returns>
+        new IList<IWeightedEdge<TVertex, TWeight>> IncomingEdges(TVertex vertex);
+
+        /// <summary>
+        /// Returns the outgoing edges of the given vertex.
+        /// </summary>
+        /// <param name="vertex">The vertex whose outgoing edges are returned.</param>
+        /// <returns>Returns a <see cref="IList{T}"/> of <see cref="IWeightedEdge{TVertex, TWeight}"/> of all outgoing edges of the given vertex.</returns>
+        new IList<IWeightedEdge<TVertex, TWeight>> OutgoingEdges(TVertex vertex);
+
+        /// <summary>
+        /// Updates the weight of the edge defined by the given existing vertices.
+        /// </summary>
+        /// <param name="firstVertex">The first vertex. Source of the edge if the graph is directed.</param>
+        /// <param name="secondVertex">The second vertex. Destination of the edge if the graph is directed.</param>
+        /// <param name="weight">The new weight of the edge.</param>
+        /// <returns>Returns true if the edge weight was updated successfully; otherwise false. Also returns false if the vertices are not present in this graph.</returns>
+        bool UpdateEdgeWeight(TVertex firstVertex, TVertex secondVertex, TWeight weight);
+    }
+}
