7516c54f7dfee466bb3a194c95c81fde8b3d359a
Alexander Donkov <abdonkov@gmail.com>
2016-10-07 15:27:00 +0300

    Added constructors for Weighted and Unweighted Edge

--- DSA/DSA/DataStructures/Graphs/UnweightedEdge.cs
+++ DSA/DSA/DataStructures/Graphs/UnweightedEdge.cs
@@ -16,13 +16,24 @@ namespace DSA.DataStructures.Graphs
         public bool IsWeighted { get { return false; } }
 
         /// <summary>
-        /// Gets the source vertex of the edge.
+        /// Gets the source vertex of the <see cref="UnweightedEdge{TVertex}"/>.
         /// </summary>
         public TVertex Source { get; internal set; }
 
         /// <summary>
-        /// Gets the destination vertex of the edge.
+        /// Gets the destination vertex of the <see cref="UnweightedEdge{TVertex}"/>.
         /// </summary>
         public TVertex Destination { get; internal set; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="UnweightedEdge{TVertex}"/> defined by the given vertices.
+        /// </summary>
+        /// <param name="source">The source vertex of the <see cref="UnweightedEdge{TVertex}"/>.</param>
+        /// <param name="destination">The destination vertex of the <see cref="UnweightedEdge{TVertex}"/>.</param>
+        public UnweightedEdge(TVertex source, TVertex destination)
+        {
+            Source = source;
+            Destination = destination;
+        }
     }
 }
--- DSA/DSA/DataStructures/Graphs/WeightedEdge.cs
+++ DSA/DSA/DataStructures/Graphs/WeightedEdge.cs
@@ -18,18 +18,30 @@ namespace DSA.DataStructures.Graphs
         public bool IsWeighted { get { return true; } }
 
         /// <summary>
-        /// Gets the source vertex of the edge.
+        /// Gets the source vertex of the <see cref="WeightedEdge{TVertex, TWeight}"/>.
         /// </summary>
         public TVertex Source { get; internal set; }
 
         /// <summary>
-        /// Gets the destination vertex of the edge.
+        /// Gets the destination vertex of the <see cref="WeightedEdge{TVertex, TWeight}"/>.
         /// </summary>
         public TVertex Destination { get; internal set; }
 
         /// <summary>
-        /// Gets the weight of the edge.
+        /// Gets the weight of the <see cref="WeightedEdge{TVertex, TWeight}"/>.
         /// </summary>
         public TWeight Weight { get; internal set; }
+
+        /// <summary>
+        /// Creates a new instance of the <see cref="WeightedEdge{TVertex, TWeight}"/> defined by the given vertices and weight.
+        /// </summary>
+        /// <param name="source">The source vertex of the <see cref="WeightedEdge{TVertex, TWeight}"/>.</param>
+        /// <param name="destination">The destination vertex of the <see cref="WeightedEdge{TVertex, TWeight}"/>.</param>
+        /// <param name="weight">The weight of the <see cref="WeightedEdge{TVertex, TWeight}"/>.</param>
+        public WeightedEdge(TVertex source, TVertex destination, TWeight weight)
+        {
+            Source = source;
+            Destination = destination;
+        }
     }
 }
