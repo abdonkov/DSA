71a830f800ebe779b2bbc81acd25aec120614f55
Alexander Donkov <abdonkov@gmail.com>
2016-10-07 14:47:26 +0300

    Added WeightedEdge and UnweightedEdge classes

---
+++ DSA/DSA/DataStructures/Graphs/UnweightedEdge.cs
@@ -0,0 +1,28 @@
+﻿using DSA.DataStructures.Interfaces;
+using System;
+
+namespace DSA.DataStructures.Graphs
+{
+    /// <summary>
+    /// Represents an unweighted edge in a graph.
+    /// </summary>
+    /// <typeparam name="TVertex">The data type of the vertices. TVertex implements <see cref="IComparable{T}"/>.</typeparam>
+    public class UnweightedEdge<TVertex> : IEdge<TVertex>
+        where TVertex : IComparable<TVertex>
+    {
+        /// <summary>
+        /// Deteremines whether the edge is weighted.
+        /// </summary>
+        public bool IsWeighted { get { return false; } }
+
+        /// <summary>
+        /// Gets the source vertex of the edge.
+        /// </summary>
+        public TVertex Source { get; internal set; }
+
+        /// <summary>
+        /// Gets the destination vertex of the edge.
+        /// </summary>
+        public TVertex Destination { get; internal set; }
+    }
+}
---
+++ DSA/DSA/DataStructures/Graphs/WeightedEdge.cs
@@ -0,0 +1,35 @@
+﻿using DSA.DataStructures.Interfaces;
+using System;
+
+namespace DSA.DataStructures.Graphs
+{
+    /// <summary>
+    /// Represents a weighted edge in a graph.
+    /// </summary>
+    /// <typeparam name="TVertex">The data type of the vertices. TVertex implements <see cref="IComparable{T}"/>.</typeparam>
+    /// <typeparam name="TWeight">The data type of the weight of the edge. TWeight implements <see cref="IComparable{T}"/>.</typeparam>
+    public class WeightedEdge<TVertex, TWeight> : IWeightedEdge<TVertex, TWeight>
+        where TVertex : IComparable<TVertex>
+        where TWeight : IComparable<TWeight>
+    {
+        /// <summary>
+        /// Deteremines whether the edge is weighted.
+        /// </summary>
+        public bool IsWeighted { get { return true; } }
+
+        /// <summary>
+        /// Gets the source vertex of the edge.
+        /// </summary>
+        public TVertex Source { get; internal set; }
+
+        /// <summary>
+        /// Gets the destination vertex of the edge.
+        /// </summary>
+        public TVertex Destination { get; internal set; }
+
+        /// <summary>
+        /// Gets the weight of the edge.
+        /// </summary>
+        public TWeight Weight { get; internal set; }
+    }
+}
