16c502e0ddf18d90a2a0c7f4c88b93fa62d17548
Alexander Donkov <abdonkov@gmail.com>
2016-10-25 20:00:20 +0300

    Added AddWeigths delegate for adding 2 edge weights
    Added a delegate defining a method for adding two edge weights and
    returning their sum.

---
+++ DSA/DSA/Algorithms/Graphs/Delegates.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace DSA.Algorithms.Graphs
+{
+    /// <summary>
+    /// A delegate defining a method that adds two graph edge weights and returns their sum.
+    /// </summary>
+    /// <typeparam name="TWeight">The data type of the weight of the edges. TWeight implements <see cref="IComparable{T}"/>.</typeparam>
+    /// <param name="a">The weight of the first item.</param>
+    /// <param name="b">The weight of the second item.</param>
+    /// <returns>Returns the sum of the edge weights.></returns>
+    public delegate TWeight AddWeights<TWeight>(TWeight a, TWeight b)
+        where TWeight : IComparable<TWeight>;
+}
