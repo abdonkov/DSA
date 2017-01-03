c7d6acf47a5ef855f1eb7a4990a7b3fcb3227363
Alexander Donkov <abdonkov@gmail.com>
2017-01-03 18:58:52 +0200

    Added TraversalMode enumerator for BST Walkers

---
+++ DSA/DSA/Algorithms/Trees/TraversalMode.cs
@@ -0,0 +1,41 @@
+﻿namespace DSA.Algorithms.Trees
+{
+    /// <summary>
+    /// Specifies the the traversal mode for the binary search tree.
+    /// </summary>
+    public enum TraversalMode
+    {
+        /// <summary>
+        /// Defines in-order, left-to-right traversal.
+        /// </summary>
+        InOrder,
+        /// <summary>
+        /// Defines in-order, right-to-left traversal.
+        /// </summary>
+        InOrderRightToLeft,
+        /// <summary>
+        /// Defines pre-order, left-to-right traversal.
+        /// </summary>
+        PreOrder,
+        /// <summary>
+        /// Defines pre-order, right-to-left traversal.
+        /// </summary>
+        PreOrderRightToLeft,
+        /// <summary>
+        /// Defines post-order, left-to-right traversal.
+        /// </summary>
+        PostOrder,
+        /// <summary>
+        /// Defines post-order, right-to-left traversal.
+        /// </summary>
+        PostOrderRightToLeft,
+        /// <summary>
+        /// Defines level-order (BFS), left-to-right traversal.
+        /// </summary>
+        LevelOrder,
+        /// <summary>
+        /// Defines level-order (BFS), right-to-left traversal.
+        /// </summary>
+        LevelOrderRightToLeft
+    }
+}
